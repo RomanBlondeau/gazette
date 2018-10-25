cf80a2c91aaf1a07ec917a9812faa5c4c4516c3a
Nicolas <nicolas1.menettrier@epitech.eu>
2018-10-25 18:38:26 +0200

    Forgot reset password (#5)
    refact forgot password

--- Sources/server/src/routes/user/post/forgotPassword.js
+++ Sources/server/src/routes/user/post/forgotPassword.js
@@ -4,37 +4,37 @@ const smtpTransport = require('../../../nodemailer');
 
 const User = require('../../../database/models/users')();
 
-router.post('/', async (req, res, next) => {
+router.post('/', (req, res, next) => {
   const { email } = req.body;
 
-  try {
-    const user = await User.findOne({ where: { email } });
+  User.findOne({ where: { email } })
+    .then(async user => {
+      if (!user) {
+        res.status(401).json({ message: 'Not found.' });
+      } else {
+        const token = crypto.randomBytes(20).toString('hex');
 
-    if (!user) {
-      res.status(401).json({ message: 'Not found.' });
-    } else {
-      const token = crypto.randomBytes(20).toString('hex');
-
-      user.updateAttributes({ reset_password: token });
-      const result = await smtpTransport.sendMail({
-        to: user.email,
-        from: process.env.userGmail,
-        template: 'forgot-password-email',
-        subject: 'Reset password token from Gazette',
-        context: {
-          name: user.username,
-          token
+        user.updateAttributes({ reset_password: token });
+        const result = await smtpTransport.sendMail({
+          to: user.email,
+          from: process.env.userGmail,
+          template: 'forgot-password-email',
+          subject: 'Reset password token from Gazette',
+          context: {
+            name: user.username,
+            token
+          }
+        });
+        if (result.rejected.length > 0) {
+          next('Not a valid mail');
+          return;
         }
-      });
-      if (result.rejected.length > 0) {
-        next('Not a valid mail');
-        return;
+        res.status(200).json({ message: `Success.` });
       }
-      res.status(200).json({ message: `Success.` });
-    }
-  } catch (err) {
-    next(err);
-  }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = { router, required: ['email'] };
