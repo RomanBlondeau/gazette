a6e5a6f032497b3863e806d928f1e862c9d16264
Roman Blondeau <roman.blondeau@epitech.eu>
2018-10-26 17:21:07 +0200

    adding lobby

--- Sources/client/app/Routes.js
+++ Sources/client/app/Routes.js
@@ -8,6 +8,11 @@ import HomePage from './containers/HomePage';
 import LoginPage from './containers/LoginPage';
 import SignUpPage from './containers/SignUpPage';
 import ForgotPassword from './containers/ForgotPasswordPage';
+import ContactsPage from './containers/ContactsPage';
+import EditPage from './containers/EditPage';
+import CalendarPage from './containers/CalendarPage';
+import HelpPage from './containers/HelpPage';
+import SettingsPage from './containers/SettingsPage';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -30,6 +35,11 @@ export default () => (
       <Route path={routes.SIGNUP} component={SignUpPage} />
       <Route path={routes.FORGOTPASSWORD} component={ForgotPassword} />
       <PrivateRoute path={routes.HOME} component={HomePage} />
+      <PrivateRoute path={routes.CONTACTS} component={ContactsPage} />
+      <PrivateRoute path={routes.EDIT} component={EditPage} />
+      <PrivateRoute path={routes.CALENDAR} component={CalendarPage} />
+      <PrivateRoute path={routes.HELP} component={HelpPage} />
+      <PrivateRoute path={routes.SETTINGS} component={SettingsPage} />
       <Route path={routes.LOGIN} component={LoginPage} />
     </Switch>
   </App>
