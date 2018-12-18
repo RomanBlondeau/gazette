0d818962f858508bc1a7457bf5a80ec8fc5b35a2
Roman Blondeau <roman.blondeau@epitech.eu>
2018-12-18 16:37:35 +0100

    [front][add] save button for project

--- Sources/client/app/components/App/NewsletterInterface/NewsletterInterface.js
+++ Sources/client/app/components/App/NewsletterInterface/NewsletterInterface.js
@@ -4,6 +4,8 @@ import { DragDropContextProvider } from 'react-dnd';
 import { connect } from 'react-redux';
 import HTML5backend from 'react-dnd-html5-backend';
 import Axios from 'axios';
+import SaveIcon from '@material-ui/icons/Save';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import { Button } from '@material-ui/core';
 import View from '../../../containers/Container/PluginViewContainer';
@@ -19,44 +21,87 @@ const mapStateToProps = state => ({
   plugins: state.container.plugins
 });
 
-function saveProject(projectId: number, rows: Array, plugins: Array) {
-  Axios.post(
-    config.projects.save,
-    {
-      container: {
-        rows,
-        plugins
-      },
-      projectId
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem('user')).token
-        }`
-      }
-    }
-  )
-    .then(res => {
-      console.log(res.message);
-    })
-    .catch(e => {
-      console.error(e);
-    });
-}
+class NewsletterInterface extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      log: ''
+    };
+  }
 
-const NewsletterInterface = ({ projectId, rows, plugins }) => (
-  <Fragment>
-    <Button type="button" onClick={() => saveProject(projectId, rows, plugins)}>
-      Save
-    </Button>
-    <View />
-    <div className={style.containerRow}>
-      <Plugin />
-      <PluginProps />
-    </div>
-  </Fragment>
-);
+  saveProject = (projectId: number, rows: Array, plugins: Array) => {
+    Axios.post(
+      config.projects.save,
+      {
+        container: {
+          rows,
+          plugins
+        },
+        projectId
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem('user')).token
+          }`
+        }
+      }
+    )
+      .then(res => {
+        this.setState({ log: 'Project saved successfully', open: true });
+        console.log(res.message);
+      })
+      .catch(e => {
+        this.setState({ log: 'An error occured, project not saved', open: true });
+        console.error(e);
+      });
+  }
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { projectId, rows, plugins } = this.props;
+    const { open, log } = this.state;
+    return (
+      <Fragment>
+        <button
+          type="button"
+          onClick={() => this.saveProject(projectId, rows, plugins)}
+          className={style.saveButton}
+        >
+          <SaveIcon style={{ fontSize: 22 }} />
+          <span style={{ position: 'relative', bottom: 7, marginLeft: 5 }}>
+            Save project
+          </span>
+        </button>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          open={open}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}
+          message={<span id="message-id">{log}</span>}
+        />
+        <View />
+        <div className={style.containerRow}>
+          <Plugin />
+          <PluginProps />
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 export default connect(
   mapStateToProps,
